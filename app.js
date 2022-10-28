07b349f822e30fc32e75becff5c8101f66932258
AbhijitTurate <abhijitturate879@gmail.com>
2022-10-29 02:16:26 +0530

    hamburger fixed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bar1=document.querySelector('.bar1');
 const bar2=document.querySelector('.bar2');
 const bar3=document.querySelector('.bar3');
 const header=document.querySelector('.header');
-
+const intro = document.querySelector(".intro")
 
 
 hamburger.addEventListener("click", () => {
@@ -21,4 +21,6 @@ hamburger.addEventListener("click", () => {
     bar2.classList.toggle('click');
     bar3.classList.toggle('click');
     header.classList.toggle('click');
+    intro.classList.toggle('click');
+
 });
